b8fedd08ce9a4608ed4b9738b4ebc747a501e632
Matthew Baggett <matthew@baggett.me>
2014-06-22 21:31:00 +0100

    Hackin' for 4sq integration

--- src/Models/FoursquareExternalAccount.php
+++ src/Models/FoursquareExternalAccount.php
@@ -8,4 +8,15 @@ class FoursquareExternalAccount extends ExternalAccount{
     return false;
   }
 
+  public function get_firstname(){
+    return $this->get_metas('firstName')->value;
+  }
+
+  public function get_lastname(){
+    return $this->get_metas('lastName')->value;
+  }
+
+  public function get_email(){
+    return json_decode($this->get_metas('contact'))->email;
+  }
 }
