7e6cbf3a66806b15e9e5fe3fbb95e7beb341c533
Matthew Baggett <matthew@baggett.me>
2014-06-23 00:11:25 +0100

    Moving from storing apikey_id to application_id on occurrences

--- eventsd.daemon.php
+++ eventsd.daemon.php
@@ -7,22 +7,13 @@ require_once("./vendor/autoload.php");
 
 require_once("./src/config/config.php");
 
-use ElephantIO\Client as Elephant;
 
 //Reduce errors
 error_reporting(~E_WARNING);
 
 Eventsd\PacketProcessor::bind_on_event_callback(function(\Eventsd\Models\Occurrence $occurrence){
   try{
-    $elephant = new Elephant('http://localhost:6948', 'socket.io', 1, false, true, true);
-    $elephant->init();
-    $elephant->send(
-      Elephant::TYPE_EVENT,
-      null,
-      null,
-      json_encode(array('name' => 'event_occurred', 'args' => $occurrence->__toArray()))
-    );
-    $elephant->close();
+    $occurrence->push();
   }catch(Exception $e){
     echo "**** EXCEPTION ****\n" . $e->getMessage() . "\n";
   }
