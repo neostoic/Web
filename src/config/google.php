7f3cf105c6b4d13295ddf4f42b449b8eb982c256
Matthew Baggett <matthew@baggett.me>
2014-06-26 12:44:04 +0100

    Updated credentials.

--- src/config/google.php
+++ src/config/google.php
@@ -1,6 +1,6 @@
 <?php
 ########## Google Settings.. Client ID, Client Secret from https://cloud.google.com/console #############
-define("GOOGLE_CLIENT_ID", '224157545807-7mkkl633fmkdkvukjs0o3j51h8fq70q9.apps.googleusercontent.com');
-define("GOOGLE_CLIENT_SECRET", 'HiVmE2VveOewXLPEaQ-YXtHP');
+define("GOOGLE_CLIENT_ID", '224157545807-nhi0ktti1iq0lhh9jo1caqv1nnb9uatv.apps.googleusercontent.com');
+define("GOOGLE_CLIENT_SECRET", 'sTZ4OaSLf-rVkCemfg-vjDBG');
 define("GOOGLE_REDIRECT_URL", 'http://intervent.io/options/external-accounts/connect/google-plus/callback'); //path to your script
 define("GOOGLE_DEVELOPER_KEY", 'AIzaSyBSXGnN_u4cZf2Ss_NzdeOjWyKH4420NnA');
