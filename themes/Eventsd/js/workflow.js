e1892a436a0a72d3e321504fcc8a7f8ee14137c7
Matthew Baggett <matthew@baggett.me>
2014-06-26 11:30:21 +0100

    Fix autoscaling workflow boxes.

--- themes/Eventsd/js/workflow.js
+++ themes/Eventsd/js/workflow.js
@@ -2,6 +2,16 @@ jQuery(document).ready(function(){
   var events = jQuery('ul.events li.event');
   var properties = jQuery('ul.properties li', events);
 
+  setTimeout(function(){
+    events.each(function(i, event){
+      var height = jQuery('.name span', event).width() + 10;
+      var heightpx = height + 'px';
+      console.log("Height: " + heightpx);
+      jQuery(this).css('min-height', heightpx);
+    });
+  }, 300);
+
+
   properties.hover(function(){
     jQuery(this).addClass('hover');
   }, function(){
