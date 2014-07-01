b0ae39d87ebe1b9a9ae61ca39f582ba779ff0b1a
Matthew Baggett <matthew@baggett.me>
2014-07-01 11:41:40 +0100

    Highcharts whee

--- src/controllers/web/status.php
+++ src/controllers/web/status.php
@@ -10,6 +10,9 @@ $app->get('/status', function () use ($app) {
     'nodejs' => (strlen(end($nodejs)) > 0)?'OKAY':'FAILED',
   );
 
+  $app->view()->addJS(WEB_ROOT . "/themes/Eventsd" . "/js/status.js");
+  $app->view()->addJS(WEB_ROOT . "/themes/Eventsd" . "/highcharts/js/modules/solid-gauge.src.js");
+
 
   $app->render('status/status.phtml', $status);
 });
