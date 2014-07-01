c263cc22887958092f720965c2840214530cde90
Matthew Baggett <matthew@baggett.me>
2014-07-01 11:29:32 +0100

    Background processify

--- eventsd.daemon.php
+++ eventsd.daemon.php
@@ -1,25 +1,39 @@
 <?php
 
-if(!file_exists('./vendor/autoload.php')){
-  die("You need to run <em>php composer.phar update</em> in the root directory.");
-}
-require_once("./vendor/autoload.php");
+//Set the ticks
+declare(ticks = 1);
 
-require_once("./src/config/config.php");
+//Fork the current process
+$processID = pcntl_fork();
 
+//Check to make sure the forked ok.
+if ( $processID == -1 ) {
+  echo "\n Error:  The process failed to fork. \n";
+} else if ( $processID ) {
+  die("Forked as $processID\n");
+  exit;
+} else {
 
-//Reduce errors
-error_reporting(~E_WARNING);
-
-Eventsd\PacketProcessor::bind_on_event_callback(function(\Eventsd\Models\Occurrence $occurrence){
-  try{
-    $occurrence->push();
-  }catch(Exception $e){
-    echo "**** EXCEPTION ****\n" . $e->getMessage() . "\n";
+  if(!file_exists('./vendor/autoload.php')){
+    die("You need to run <em>php composer.phar update</em> in the root directory.");
   }
-});
+  require_once("./vendor/autoload.php");
 
-Eventsd\PacketProcessor::create_socket_server(EVENTSD_BIND, EVENTSD_PORT);
+  require_once("./src/config/config.php");
 
 
+  //Reduce errors
+  error_reporting(~E_WARNING);
+
+  Eventsd\PacketProcessor::bind_on_event_callback(function(\Eventsd\Models\Occurrence $occurrence){
+    try{
+      $occurrence->push();
+    }catch(Exception $e){
+      echo "**** EXCEPTION ****\n" . $e->getMessage() . "\n";
+    }
+  });
+
+  Eventsd\PacketProcessor::create_socket_server(EVENTSD_BIND, EVENTSD_PORT);
+
+}
 
