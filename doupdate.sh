878ac5337c7b1ad75737aa55003c410b486b2fa1
Matthew Baggett <matthew@baggett.me>
2014-07-08 11:57:35 +0100

    Do update

--- doupdate.sh
+++ doupdate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+killall -9 php;
+killall -9 nodejs;
 git reset --hard HEAD;
 git pull;
 git submodule init;
 git submodule update;
 php composer.phar update;
-killall -9 php;
-killall -9 nodejs;
 nodejs realtime.js &
 php eventsd.daemon.php &
 php eventsd.packet-processor.php &
